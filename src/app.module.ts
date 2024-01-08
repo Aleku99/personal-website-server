6a5ecc48883581c5fbe942e0737d6994f70ba8eb
Alexandru Loghin <loghinalex19@gmail.com>
2024-01-08 20:07:03 +0200

    feat: added Cron job
    Signed-off-by: Alexandru Loghin <loghinalex19@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MessageModule } from './message/message.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ScheduleModule } from '@nestjs/schedule';
+import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [MessageModule, PrismaModule],
+  imports: [ScheduleModule.forRoot(), MessageModule, PrismaModule, EmailModule],
 })
 export class AppModule {}
