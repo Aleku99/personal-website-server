743fa1d8afbb5ae2cb65e1e22e73521e19238c5f
Alexandru Loghin <loghinalex19@gmail.com>
2024-01-12 22:04:40 +0200

    feat: added logic to the cron handler function to send e-mails every 30 minutes
    Signed-off-by: Alexandru Loghin <loghinalex19@gmail.com>

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,12 +1,28 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
+import { MailerService } from '@nestjs-modules/mailer';
+import { MessageService } from 'src/message/message.service';
 
 @Injectable()
 export class EmailService {
-  private readonly logger = new Logger(EmailService.name);
+  constructor(
+    private mailerService: MailerService,
+    private messageService: MessageService,
+  ) {}
 
-  @Cron(CronExpression.EVERY_10_SECONDS)
-  handleCron() {
-    this.logger.debug('Called every 10 seconds');
+  @Cron(CronExpression.EVERY_30_MINUTES)
+  async handleCron() {
+    const emails = await this.messageService.getUnsentMessages();
+
+    emails.forEach(async (email) => {
+      const res = await this.mailerService.sendMail({
+        to: 'loghinalex19@gmail.com', // list of receivers
+        subject: 'New Personal Website E-mail', // Subject line
+        html: `<p>Got new message from ${email.name}, e-mail ${email.email}</p> <p style="padding:12px;border-left:4px solid #d0d0d0;font-style:italic">${email.message}</p> `,
+      });
+      if (res.response.includes('250')) {
+        this.messageService.markAsSent(email.id);
+      }
+    });
   }
 }
